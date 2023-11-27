4f2a82bcdae92916ff7d9cd8187d826a85a28b15
germanolsd <germanolsd@gmail.com>
2023-11-27 14:56:07 -0300

    Update package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
-
-export default defineConfig({
+const base = {
   plugins: [vue()],
   server: {
     port: 3001
@@ -13,23 +11,37 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, '/src')
     }
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/entry/index.js'),
-      name: 'FancyInOut',
-      fileName: 'index'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      input: process.env.MODE === 'npm' ? 'src/entry/index.js' : 'index.html',
-      output: {
-        dir: process.env.MODE === 'npm' ? 'dist/npm' : 'dist/deploy',
-        format: 'es',
-        globals: {
-          vue: 'Vue'
+  }
+}
+
+// https://vitejs.dev/config/
+
+export default defineConfig(({mode}) => {
+  let build;
+  if (mode === 'npm') {
+    build = {
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'src/entry/index.js'),
+          name: 'FancyInOut',
+          fileName: 'index'
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue'
+            }
+          }
         }
       }
     }
+  }
+  if (mode === 'deploy') {
+    build = {}
+  }
+  return {
+    ...base,
+    ...build
   }
 })
